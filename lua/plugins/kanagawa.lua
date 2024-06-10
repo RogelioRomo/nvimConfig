18b4d5b717f9a0f7400cd43324b5381d473aff57
rogelioromo <rogelioromo25@gmail.com>
2024-06-09 19:28:25 -0700

    final setup for nvim

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,17 +1,16 @@
-return { 
-    "rebelot/kanagawa.nvim", 
-    name = "kanagawa", 
-    priority = 1000,
-    config = function()
-        require('kanagawa').setup({
-            overrides = function(colors)
-                return {
-                    LineNr = { bg = "NONE" },
-                    CursorLineNr = { bg = "NONE" }
-                }
-            end
-        })
-        vim.cmd.colorscheme "kanagawa-dragon"
-    end
+return {
+	"rebelot/kanagawa.nvim",
+	name = "kanagawa",
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			overrides = function(colors)
+				return {
+					LineNr = { bg = "NONE" },
+					CursorLineNr = { bg = "NONE" },
+				}
+			end,
+		})
+		vim.cmd.colorscheme("kanagawa-dragon")
+	end,
 }
-
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "tsserver", "yamlls", "marksman", "jsonls", "html", "cssls"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.yamlls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n' }, '<space>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "tsserver", "yamlls", "marksman", "jsonls", "html", "cssls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<space>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
